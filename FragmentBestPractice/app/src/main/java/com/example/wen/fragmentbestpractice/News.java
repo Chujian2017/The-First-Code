885522bf6bf5af58000b4abf09dad28b007b3603
Chujian2017 <qianmo@mail.dlut.edu.cn>
2018-01-31 23:25:31 +0800

    碎片的应用

---
+++ FragmentBestPractice/app/src/main/java/com/example/wen/fragmentbestpractice/News.java
@@ -0,0 +1,23 @@
+package com.example.wen.fragmentbestpractice;
+
+/**
+ * Created by wen on 2018/1/31.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title=title;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+    public String getContent(){
+        return content;
+    }
+
+}

d5c3605c7aa08aa5517ed21ab4d19c69630fee50
Chujian2017 <qianmo@mail.dlut.edu.cn>
2018-02-07 15:27:26 +0800

    添加数据库查询，插入，更新，删除

--- DatabaseTest/app/src/main/java/com/example/wen/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/wen/databasetest/MainActivity.java
@@ -1,23 +1,90 @@
 package com.example.wen.databasetest;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static  final String TAG="MainActivity";
     private MyDatebaseHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dbHelper=new MyDatebaseHelper(this,"BookStore.db",null,1);
-        Button creatDatabase=(Button)findViewById(R.id.create_database);
+        dbHelper = new MyDatebaseHelper(this, "BookStore.db", null, 2);
+        Button creatDatabase = (Button) findViewById(R.id.create_database);
+        Button addData = (Button) findViewById(R.id.add_data);
+        Button updateData=(Button)findViewById(R.id.update_data);
+        Button deleteButton=(Button)findViewById(R.id.delete_data);
+        Button queryButton=(Button)findViewById(R.id.query_data);
         creatDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 dbHelper.getWritableDatabase();
+            }
+        });
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                //开始组装第一条数据
+                values.put("name", "The Da Vinci Code");
+                values.put("author", "Dan Brown");
+                values.put("pages", 454);
+                values.put("price", 16.96);
+                db.insert("Book", null, values);//插入第一条数据
+                values.clear();
+
+                values.put("name", "The Lost Symbol");
+                values.put("author", "Dan Brown");
+                values.put("pages", 510);
+                values.put("price", 19.95);
+                db.insert("Book", null, values);
+
+            }
+        });
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db=dbHelper.getWritableDatabase();
+                ContentValues values=new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name =?",new String[]{"The Da Vinci Code"});
+            }
+        });
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db=dbHelper.getWritableDatabase();
+                db.delete("Book","Pages>?",new String[]{"500"});
+            }
+        });
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase db=dbHelper.getWritableDatabase();
+                //查询Book表中所有的数据
+                Cursor cursor=db.query("Book",null,null,null,null,null,null);
+                if(cursor.moveToFirst()){
+                    do{
+                        //遍历Cursor对象,取出数据并打印
+                        String name=cursor.getString(cursor.getColumnIndex("name"));
+                        String author=cursor.getString(cursor.getColumnIndex("author"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d(TAG, "book name is  "+name);
+                        Log.d(TAG, "book author is  "+author);
+                        Log.d(TAG, "book page is  "+pages);
+                        Log.d(TAG, "book price is  "+price);
+                    }while(cursor.moveToNext());
+                }cursor.close();
             }
         });
     }
--- DatabaseTest/app/src/main/java/com/example/wen/databasetest/MyDatebaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/wen/databasetest/MyDatebaseHelper.java
@@ -16,6 +16,10 @@ public class MyDatebaseHelper extends SQLiteOpenHelper {
             +"price real,"
             +"pages intger,"
             +"name text)";
+    public static final String CREATE_CATEGORY="create table Category("
+            +"id integer primary key autoincrement,"
+            +"category_name text,"
+            +"category_code integer)";
     private Context mContext;
     public MyDatebaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,int version){
         super(context,name,factory,version);
@@ -25,11 +29,14 @@ public class MyDatebaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
     }
 }

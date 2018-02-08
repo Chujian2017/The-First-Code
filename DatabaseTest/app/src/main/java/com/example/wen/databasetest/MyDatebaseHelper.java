e2a7ca57ea944ec4c574c6cc5a4ba26a933791f7
Chujian2017 <qianmo@mail.dlut.edu.cn>
2018-02-08 23:45:12 +0800

    内容提供器

--- DatabaseTest/app/src/main/java/com/example/wen/databasetest/MyDatebaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/wen/databasetest/MyDatebaseHelper.java
@@ -30,7 +30,7 @@ public class MyDatebaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
+        //Toast.makeText(mContext,"Create succeeded",Toast.LENGTH_SHORT).show();
     }
 
     @Override

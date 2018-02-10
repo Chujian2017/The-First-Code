10b1d93e6fb6f8e4d0b94f8e770613ae7b36067a
Chujian2017 <qianmo@mail.dlut.edu.cn>
2018-02-10 16:04:38 +0800

    添加选择音乐功能

--- PlayAudioTest/app/src/main/java/com/example/wen/playaudiotest/MainActivity.java
+++ PlayAudioTest/app/src/main/java/com/example/wen/playaudiotest/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.wen.playaudiotest;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.ContentUris;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
-import android.os.Environment;
-import android.support.annotation.NonNull;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +19,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private MediaPlayer mediaPlayer=new MediaPlayer();
+    private static final int CHOOSE_SONG=2;
+    private String musicPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,20 +32,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button play=(Button)findViewById(R.id.play);
         Button pause=(Button)findViewById(R.id.pause);
         Button stop=(Button)findViewById(R.id.stop);
+        Button select=(Button)findViewById(R.id.select);
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
-        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
-        }else{
-            initMediaPlayer();//初始化MediaPlayer
-        }
+        select.setOnClickListener(this);
+
     }
-    private void initMediaPlayer(){
+    private void initMediaPlayer(String path){
         try{
-            File file=new File(Environment.getExternalStorageDirectory(),"music.mp3");
-            mediaPlayer.setDataSource(file.getPath());
+            mediaPlayer.setDataSource(path);
             mediaPlayer.prepare();
         }catch (Exception e){
             e.printStackTrace();
@@ -46,11 +49,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode,  String[] permissions,  int[] grantResults) {
         switch (requestCode){
             case 1:
                 if(grantResults.length>0&&grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    initMediaPlayer();
+                    openMusic();
                 }else{
                     Toast.makeText(this,"拒绝权限将无法使用程序",
                     Toast.LENGTH_SHORT).show();
@@ -77,14 +80,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop:
                 if(mediaPlayer.isPlaying()){
                     mediaPlayer.reset();;
-                    initMediaPlayer();
+                    initMediaPlayer(musicPath);
                 }
                 break;
+            case R.id.select:
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
+                } else {
+                    openMusic();
+                }
             default:
                 break;
         }
     }
-
+    private void openMusic() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("audio/*");
+        startActivityForResult(intent, CHOOSE_SONG); // 打开音乐盒
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -93,4 +107,65 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             mediaPlayer.release();
         }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case CHOOSE_SONG:
+                if (resultCode == RESULT_OK) {
+                    // 判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        // 4.4及以上系统使用这个方法处理图片
+                        handleMusic(data);
+                    } else {
+                        // 4.4以下系统使用这个方法处理图片
+                        handleMusicBefore(data);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    @TargetApi(19)
+    private void handleMusic(Intent data) {
+        String musicPath = null;
+        Uri uri = data.getData();
+        if (DocumentsContract.isDocumentUri(this, uri)) {
+            // 如果是document类型的Uri，则通过document id处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())) {
+                String id = docId.split(":")[1]; // 解析出数字格式的id
+                String selection = MediaStore.Audio.Media._ID + "=" + id;
+                musicPath = getMusicPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, selection);
+            } else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())) {
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(docId));
+                musicPath = getMusicPath(contentUri, null);
+            }
+        } else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是content类型的Uri，则使用普通方式处理
+            musicPath = getMusicPath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            // 如果是file类型的Uri，直接获取图片路径即可
+            musicPath = uri.getPath();
+        }
+        initMediaPlayer(musicPath);
+    }
+    private void handleMusicBefore(Intent data) {
+        Uri uri = data.getData();
+        String musicPath = getMusicPath(uri, null);
+        initMediaPlayer(musicPath);
+    }
+    private String getMusicPath(Uri uri, String selection) {
+        musicPath = null;
+        Cursor cursor = getContentResolver().query(uri, null, selection, null, null);
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                musicPath = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+            }
+            cursor.close();
+        }
+        return musicPath;
+    }
+
 }
